c37855514ec39465459b8999201805656f4e3d85
Mark Kpamy <markkpamy@gmail.com>
2018-12-11 09:48:19 +0100

    update classe agent negociateur

--- Negociation/src/main/java/com/sma/negociation/demo/agent/AgentNegociateur.java
+++ Negociation/src/main/java/com/sma/negociation/demo/agent/AgentNegociateur.java
@@ -1,18 +1,32 @@
 package com.sma.negociation.demo.agent;
 
+import com.sma.negociation.demo.strategies.StrategieNegociateur;
+
 public class AgentNegociateur extends Agent {
-    public AgentNegociateur() {
+    private StrategieNegociateur strategieNegociateur;
+
+    public AgentNegociateur(StrategieNegociateur strategieNegociateur) {
         super();
+        this.strategieNegociateur = strategieNegociateur;
+    }
+
+    public StrategieNegociateur getStrategieNegociateur() {
+        return strategieNegociateur;
+    }
+
+    public void setStrategieNegociateur(StrategieNegociateur strategieNegociateur) {
+        this.strategieNegociateur = strategieNegociateur;
     }
 
     @Override
     public void run() {
         long temps_dep_neg = System.currentTimeMillis();
-        // condition d'arr
-        // while
-        while(){
-
+        long temps_courant = 0;
+        // while condition d'arr
+        while (true) {
+            //calculer la diff de temps avt l'envoi du message pour savoir si on stoppe
+            temps_courant = System.currentTimeMillis();
         }
-        long temps_courant = System.currentTimeMillis();
+
     }
 }
