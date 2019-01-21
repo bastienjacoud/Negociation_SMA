ce067e33d95515bbcc82eab39f12286a8c14693c
bastienjacoud <bastien.jacoud@hotmail.fr>
2019-01-21 16:37:33 +0100

    Logger fini

--- Negociation/src/main/java/com/sma/negociation/demo/DemoApplication.java
+++ Negociation/src/main/java/com/sma/negociation/demo/DemoApplication.java
@@ -2,13 +2,14 @@ package com.sma.negociation.demo;
 
 import com.sma.negociation.demo.logger.MyLogger;
 import com.sma.negociation.demo.regles.Concession;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
-
     public static void main(String[] args) {
+
         MyLogger.logInfo("ça fonctionne");
         MyLogger.logError("Il y a une erreur");
         MyLogger.logWarning("Avertissement !!!");
--- Negociation/src/main/java/com/sma/negociation/demo/logger/MyLogger.java
+++ Negociation/src/main/java/com/sma/negociation/demo/logger/MyLogger.java
@@ -1,41 +1,20 @@
 package com.sma.negociation.demo.logger;
 
 
-import java.io.IOException;
-import java.util.logging.FileHandler;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import java.util.logging.SimpleFormatter;
-import static org.springframework.boot.logging.LoggingSystemProperties.LOG_FILE;
-
 public class MyLogger {
 
-    private static Logger logger  = getInstance();
-
-    private static Logger getInstance(){
-        logger = Logger.getLogger(LOG_FILE);
-        FileHandler fh;
-        try{
-            // This block configure the logger with handler and formatter
-            fh = new FileHandler(LOG_FILE);
-            logger.addHandler(fh);
-            SimpleFormatter formatter = new SimpleFormatter();
-            fh.setFormatter(formatter);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return logger;
-    }
-
     public static void logInfo(String log){
-        logger.log(Level.INFO, log);
+        ConsoleLogger.logInfo(log);
+        FileLogger.logInfo(log);
     }
 
     public static void logError(String log){
-        logger.log(Level.SEVERE, log);
+        ConsoleLogger.logError(log);
+        FileLogger.logError(log);
     }
 
     public static void logWarning(String log){
-        logger.log(Level.WARNING, log);
+        ConsoleLogger.logWarning(log);
+        FileLogger.logWarning(log);
     }
 }
